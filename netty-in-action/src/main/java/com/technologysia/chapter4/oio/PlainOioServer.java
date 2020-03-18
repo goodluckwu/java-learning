77b197898a608ba34bf404eb353d3e1e418aec25
house <wuzhihao7@outlook.com>
2020-03-18 22:05:59 +0800

    netty-in-action代码

--- netty-in-action/src/main/java/com/technologysia/chapter4/oio/PlainOioServer.java
+++ netty-in-action/src/main/java/com/technologysia/chapter4/oio/PlainOioServer.java
@@ -1,4 +1,4 @@
-package com.technologysia;
+package com.technologysia.chapter4.oio;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,21 +13,25 @@ public class PlainOioServer {
     private static final Logger log = LoggerFactory.getLogger(PlainOioServer.class);
 
     public void serve(int port) throws IOException {
+        //将服务器绑定到指定端口
         final ServerSocket serverSocket = new ServerSocket(port);
             for(;;){
+                //接受连接
                 Socket socket = serverSocket.accept();
                 log.info("Accepted connection from {}", socket);
+                //创建一个新的线程来处理该连接
                 new Thread(() -> {
                     OutputStream out;
                     try {
                         out = socket.getOutputStream();
+                        //将消息写给已连接的客户端
                         out.write("Hello, World!\r\n".getBytes(StandardCharsets.UTF_8));
                         out.flush();
-                        socket.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }finally {
                         try {
+                            //关闭连接
                             socket.close();
                         } catch (IOException e) {
                             e.printStackTrace();
