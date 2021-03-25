520187e54fc5a499409ff6059562e5a082b8dc67
haoyoungde <wuzhihao7@outlook.com>
2021-03-25 21:39:53 +0800

    netty discard

--- netty-learning/src/main/java/io/github/wuzhihao7/netty/discard/DiscardClient.java
+++ netty-learning/src/main/java/io/github/wuzhihao7/netty/discard/DiscardClient.java
@@ -1,58 +1,54 @@
 package io.github.wuzhihao7.netty.discard;
 
-import io.netty.bootstrap.ServerBootstrap;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.bootstrap.BootstrapConfig;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 
-public class DiscardServer {
+import java.awt.image.BufferStrategy;
+
+public class DiscardClient {
     static final boolean SSL = System.getProperty("ssl") != null;
+    static final String HOST = System.getProperty("host", "127.0.0.1");
     static final int PORT = Integer.parseInt(System.getProperty("port", "8090"));
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
 
     public static void main(String[] args) throws Exception {
         final SslContext sslCtx;
         if(SSL){
-            SelfSignedCertificate ssc = new SelfSignedCertificate();
-            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
-        }else {
+            sslCtx = SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).build();
+        }else{
             sslCtx = null;
         }
 
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ChannelPipeline pipeline = ch.pipeline();
                             if(sslCtx != null){
-                                pipeline.addLast(sslCtx.newHandler(ch.alloc()));
+                                pipeline.addLast(sslCtx.newHandler(ch.alloc(), HOST, PORT));
                             }
-                            pipeline.addLast(new DiscardServerHandler());
                         }
                     });
 
-            ChannelFuture f = b.bind(PORT).sync();
+            ChannelFuture f = b.connect(HOST, PORT).sync();
 
             f.channel().closeFuture().sync();
-
-        }finally {
+        }finally{
             workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
         }
     }
 }
