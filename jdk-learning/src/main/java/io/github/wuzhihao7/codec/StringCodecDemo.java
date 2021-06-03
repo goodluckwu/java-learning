2fb4dca5ad3e7610d39c755891f41f8310961b25
wuzhihao <wuzhihao@eptok.com>
2021-06-03 10:47:28 +0800

    jdk codec

--- jdk-learning/src/main/java/io/github/wuzhihao7/codec/StringCodecDemo.java
+++ jdk-learning/src/main/java/io/github/wuzhihao7/codec/StringCodecDemo.java
@@ -1,14 +1,43 @@
 package io.github.wuzhihao7.codec;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 
 public class StringCodecDemo {
-    public static void main(String[] args) {
-        unicode();
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        String str = "我爱中国 China";
+        System.out.println("转unicode");
+        unicode(str);
+        System.out.println("转utf8");
+        utf8(str);
+        gb2312(str);
     }
 
-    public static void unicode(){
-        String str = "我爱中国 China";
+    private static void gb2312(String str) throws UnsupportedEncodingException {
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            byte[] bytes = (c + "").getBytes("GB2312");
+            String tmp = "";
+            for(byte b : bytes){
+                tmp += Integer.toHexString(b & 0x00ff) + " ";
+            }
+            System.out.printf("%s %s%n", c, tmp);
+        }
+    }
+
+    private static void utf8(String str) {
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            byte[] bytes = (c + "").getBytes(StandardCharsets.UTF_8);
+            String tmp = "";
+            for(byte b : bytes){
+                tmp += Integer.toHexString(b & 0x00ff) + " ";
+            }
+            System.out.printf("%s %s%n", c, tmp);
+        }
+    }
+
+    public static void unicode(String str){
         for(int i=0; i < str.length(); i++){
             char c = str.charAt(i);
             System.out.printf("%c %s%n", c, Integer.toHexString(c));
--- third-lib-learning/src/main/java/io/github/wuzhihao7/commonstext/StringSubstitutorDemo.java
+++ third-lib-learning/src/main/java/io/github/wuzhihao7/commonstext/StringSubstitutorDemo.java
@@ -2,16 +2,10 @@ package io.github.wuzhihao7.commonstext;
 
 
 import org.apache.commons.text.StringSubstitutor;
-import org.apache.commons.text.lookup.StringLookup;
 
 public class StringSubstitutorDemo {
     public static void main(String[] args) {
-        StringSubstitutor stringSubstitutor = new StringSubstitutor(new StringLookup() {
-            @Override
-            public String lookup(String key) {
-                return System.getProperty(key);
-            }
-        });
+        StringSubstitutor stringSubstitutor = new StringSubstitutor(System::getProperty);
         System.out.println(stringSubstitutor.replace("${user.home}"));
     }
 }
